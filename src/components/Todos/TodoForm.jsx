e1fb5841e97c166f6b17850b9b705a7f989634e6
WariusFox <badboy101@bk.ru>
2024-06-07 20:15:08 +0400

    Todo App V1

---
+++ src/components/Todos/TodoForm.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useState } from "react";
+import styles from "./TodoForm.module.css";
+
+function TodoForm(props) {
+  const { addTodo } = props;
+  const [text, setText] = useState("");
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    addTodo(text);
+    setText("");
+  };
+
+  return (
+    <div className={styles.todoFormContainer}>
+      <form onSubmit={onSubmitHandler}>
+        <input
+          placeholder="Endet new Todo"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default TodoForm;
